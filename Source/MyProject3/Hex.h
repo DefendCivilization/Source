8fc86725e7399216bfdcf98f802c66ec48c49918
adejong007 <andrew.dejong5@gmail.com>
2015-03-18 00:45:27 -0400

    Added tiles as Sprites

---
+++ Source/MyProject3/Hex.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "Hex.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT3_API UHex : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
